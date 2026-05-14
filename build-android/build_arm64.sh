34e6e79dbfbd3c70e6f1df6dbce768b5f8666051
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2026-05-14 12:17:32 +0400

    build: sync API level toolchain

--- build-android/build_arm64.sh
+++ build-android/build_arm64.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export TARGET_TRIPLE=aarch64-linux-android34
+export TARGET_TRIPLE=aarch64-linux-android29
 export MESON_CPU_FAMILY=aarch64
 export MESON_CPU=armv8
 export EXTRA_ARGS="-Dfreedreno-kmds=kgsl -Dgallium-drivers=zink,freedreno -Dvulkan-drivers=freedreno"
--- build-android/build_x86.sh
+++ build-android/build_x86.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export TARGET_TRIPLE=i686-linux-android26
+export TARGET_TRIPLE=i686-linux-android29
 export MESON_CPU_FAMILY=x86
 export MESON_CPU=i686
 export EXTRA_ARGS="-Dgallium-drivers=zink -Dvulkan-drivers= "
